ba2ebe01ef73150f6e7908cafc6a72a58ff51c8e
nardielloam <nardielloam@gmail.com>
2020-10-16 11:41:52 -0600

    Updates the third and second homework problem

--- src/test/java/edu/montana/csci/csci440/homework/Homework3.java
+++ src/test/java/edu/montana/csci/csci440/homework/Homework3.java
@@ -36,8 +36,8 @@ public class Homework3 extends DBTest {
      */
     public void createGrammyInfoTable(){
         //TODO fill these in
-        executeDDL("CREATE TABLE grammy_categories( Name NVARCHAR(255) NOT NULL, GrammyCategoryId INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY(GrammyCategoryId)");
-        executeDDL("CREATE TABLE grammy_infos( ArtistId INTEGER NOT NULL, AlbumId INTEGER, TrackId INTEGER, GrammyCategoryId INTEGER NOT NULL, Status NVARCHAR(255)");
+        executeDDL("CREATE TABLE grammy_categories( Name NVARCHAR(255) NOT NULL, GrammyCategoryId INTEGER NOT NULL AUTOINCREMENT, PRIMARY KEY(GrammyCategoryId))");
+        executeDDL("CREATE TABLE grammy_infos( ArtistId INTEGER NOT NULL, AlbumId INTEGER, TrackId INTEGER, GrammyCategoryId INTEGER NOT NULL, Status NVARCHAR(255) PRIMARY KEY(STATUS), FOREIGN KEY(GrammyCategoryId), )");
 
         // TEST CODE
         executeUpdate("INSERT INTO grammy_categories(Name) VALUES ('Greatest Ever');");
@@ -61,7 +61,12 @@ public class Homework3 extends DBTest {
         Integer before = (Integer) executeSQL("SELECT COUNT(*) as COUNT FROM genres").get(0).get("COUNT");
 
         //TODO fill this in
-        executeUpdate("INSERT");
+        executeUpdate("INSERT INTO genres(Name) VALUES " +
+                "'House'" +
+                "'Trance'" +
+                "'Dubstep'" +
+                "'Future House'" +
+                "'Trance House'");
 
         Integer after = (Integer) executeSQL("SELECT COUNT(*) as COUNT FROM genres").get(0).get("COUNT");
         assertEquals(before + 5, after);
