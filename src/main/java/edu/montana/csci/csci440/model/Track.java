ff77e20ef11f669e06b176f67c1caadcef94fa33
nardielloam <nardielloam@gmail.com>
2020-11-06 10:11:37 -0700

    adds changes to get search working on tracks

--- src/main/java/edu/montana/csci/csci440/model/Track.java
+++ src/main/java/edu/montana/csci/csci440/model/Track.java
@@ -279,12 +279,13 @@ public class Track extends Model {
     }
 
     public static List<Track> search(int page, int count, String orderBy, String search) {
-        String query = "SELECT * FROM tracks WHERE name LIKE ? LIMIT ?";
+        String query = "SELECT * FROM tracks JOIN albums on tracks.AlbumId = albums.AlbumId WHERE name LIKE ? GROUP BY "+orderBy+" LIMIT ? OFFSET ?";
         search = "%" + search + "%";
         try (Connection conn = DB.connect();
              PreparedStatement stmt = conn.prepareStatement(query)) {
             stmt.setString(1, search);
             stmt.setInt(2, count);
+            stmt.setInt(3, count*(page-1));
             ResultSet results = stmt.executeQuery();
             List<Track> resultList = new LinkedList<>();
             while (results.next()) {
